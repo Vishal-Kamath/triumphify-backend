a270e0f49c73aabd1c2fe8fd017137dd56f446b1
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-10 16:46:37 +0530

    ⬆️update: rename endpoint

--- src/app/entities/products/controllers/index.products.controllers.ts
+++ src/app/entities/products/controllers/index.products.controllers.ts
@@ -2,7 +2,7 @@ import handleGetProductMeta from "./get.meta.products.controllers";
 import handleGetProduct from "./get.product.controller";
 import handleGetProductShowcase from "./get.showcase.products.controller";
 import handleGetProductBuyDetails from "./get.product-buy.controllers";
-import handleCreateUserProductReview from "./add.user.review.products.controller";
+import handleCreateUserProductReview from "./create.user.review.products.controller";
 
 export default {
   handleGetProductMeta,
