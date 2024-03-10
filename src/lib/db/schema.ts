6220b9325130b5ddd24ace4b7917e5920d791d16
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-10 16:39:29 +0530

    ➕add: review unique key

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,3 +1,4 @@
+import { table } from "console";
 import {
   mysqlTable,
   timestamp,
@@ -393,19 +394,29 @@ export type DbOrders = typeof orders.$inferSelect;
 // -------------------------------------------------
 // Reviews
 // -------------------------------------------------
-export const reviews = mysqlTable("reviews", {
-  id: varchar("id", { length: 36 }).notNull().primaryKey(),
-  user_id: varchar("user_id", { length: 36 })
-    .notNull()
-    .references(() => users.id),
-  product_id: varchar("product_id", { length: 36 })
-    .notNull()
-    .references(() => products.id),
+export const reviews = mysqlTable(
+  "reviews",
+  {
+    user_id: varchar("user_id", { length: 36 })
+      .notNull()
+      .references(() => users.id),
+    product_id: varchar("product_id", { length: 36 })
+      .notNull()
+      .references(() => products.id),
 
-  rating: int("rating").notNull(),
-  review_title: varchar("review_title", {length: 100}).notNull(),
-  review_description: varchar("review_description", {length: 750}).notNull(),
+    rating: int("rating").notNull(),
+    review_title: varchar("review_title", { length: 100 }).notNull(),
+    review_description: varchar("review_description", {
+      length: 750,
+    }).notNull(),
 
-  created_at: timestamp("created_at").notNull().defaultNow(),
-  updated_at: timestamp("updated_at").onUpdateNow(),
-});
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at").onUpdateNow(),
+  },
+  (table) => ({
+    unique_user_product_id: primaryKey({
+      name: "unique_user_product_id",
+      columns: [table.user_id, table.product_id],
+    }),
+  })
+);
