90259eef1fd7a5ea02d10ebea5d09a4fdbb56399
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-17 14:33:38 +0530

    ❌Remove old admin

--- src/admin-old/entities/orders/routes.orders.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import orderControllers from "./controllers/index.orders.controllers";
-
-const router = Router();
-
-router.route("/").get(orderControllers.handleGetAllOrders);
-router.route("/:orderId").get(orderControllers.handleGetByIdOrdersControllers);
-
-export default router;
--- src/admin-old/utils/getRole.ts
+++
@@ -1,8 +0,0 @@
-import { env } from "@/config/env.config";
-
-export const getRole = (role: string) => {
-  if (role === env.ADMIN) return "admin";
-  if (role === env.SUPERADMIN) return "superadmin";
-  if (role === env.EMPLOYEE) return "employee";
-  return "employee";
-};
