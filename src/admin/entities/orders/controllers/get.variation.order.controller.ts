284d3e9f20c8f08cb9bb28c081dee9f92b80269a
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-26 12:18:42 +0530

    ➕Add: variation sales with type

--- src/admin/entities/orders/controllers/get.variation.order.controller.ts
+++ src/admin/entities/orders/controllers/get.variation.order.controller.ts
@@ -1,11 +1,16 @@
 import { db } from "@/lib/db";
 import { orders, product_variations } from "@/lib/db/schema";
 import { Logger } from "@/utils/logger";
-import { and, eq, gte, lte, sum } from "drizzle-orm";
+import { and, eq, gte, lte, or, sum } from "drizzle-orm";
 import { Request, Response } from "express";
 
 const handleGetVariationSales = async (
-  req: Request<{ productId: string }, {}, {}, { start: string; end: string }>,
+  req: Request<
+    { productId: string },
+    {},
+    {},
+    { start: string; end: string; type: "history" | "cancelled" | "returned" }
+  >,
   res: Response
 ) => {
   try {
@@ -16,6 +21,41 @@ const handleGetVariationSales = async (
       : new Date(
           new Date(end_date).setMonth(new Date(end_date).getMonth() - 1)
         );
+
+    const type = req.query.type;
+    const condition =
+      type === "history"
+        ? or(
+            and(
+              eq(orders.product_id, productId),
+              gte(orders.created_at, start_date),
+              lte(orders.created_at, end_date),
+              eq(orders.cancelled, false),
+              eq(orders.returned, false)
+            ),
+            and(
+              eq(orders.product_id, productId),
+              gte(orders.created_at, start_date),
+              lte(orders.created_at, end_date),
+              eq(orders.cancelled, true),
+              eq(orders.returned, true)
+            )
+          )
+        : type === "cancelled"
+        ? and(
+            eq(orders.product_id, productId),
+            gte(orders.created_at, start_date),
+            lte(orders.created_at, end_date),
+            eq(orders.cancelled, true),
+            eq(orders.returned, false)
+          )
+        : and(
+            eq(orders.product_id, productId),
+            gte(orders.created_at, start_date),
+            lte(orders.created_at, end_date),
+            eq(orders.cancelled, false),
+            eq(orders.returned, true)
+          );
 
     const variation_sales = await db
       .select({
@@ -28,13 +68,7 @@ const handleGetVariationSales = async (
         total_sales: sum(orders.product_variation_final_price),
       })
       .from(orders)
-      .where(
-        and(
-          eq(orders.product_id, productId),
-          gte(orders.created_at, start_date),
-          lte(orders.created_at, end_date)
-        )
-      )
+      .where(condition)
       // .fullJoin(
       //   product_variations,
       //   eq(
@@ -43,7 +77,7 @@ const handleGetVariationSales = async (
       //   )
       // )
       .groupBy((data) => [data.variation_key]);
-
+    console.log(variation_sales);
     return res.status(200).send({ data: variation_sales, type: "success" });
   } catch (err) {
     Logger.error("handle get variation sales", err);
