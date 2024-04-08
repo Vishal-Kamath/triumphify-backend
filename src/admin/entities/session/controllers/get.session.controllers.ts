6b8daa3bc5ed980737dd6794e5428a59f6af56a1
Vishal-Kamath <vishalkamath853@gmail.com>
2024-04-08 14:17:24 +0530

    🪲fix: date padding

--- src/admin/entities/session/controllers/get.session.controllers.ts
+++ src/admin/entities/session/controllers/get.session.controllers.ts
@@ -2,8 +2,9 @@ import { TokenPayload } from "@/admin/utils/jwt.utils";
 import { db } from "@/lib/db";
 import { employee_time_session } from "@/lib/db/schema";
 import { Logger } from "@/utils/logger";
-import { and, eq, sum } from "drizzle-orm";
+import { and, asc, eq, sum } from "drizzle-orm";
 import { Request, Response } from "express";
+import { orderBy } from "lodash";
 
 const handleGetSession = async (
   req: Request<{}, {}, TokenPayload>,
@@ -12,20 +13,35 @@ const handleGetSession = async (
   try {
     const { id } = req.body.token;
 
-    const sessions = await db
-      .select({
-        date: employee_time_session.date,
-        time: sum(employee_time_session.time),
-      })
-      .from(employee_time_session)
-      .where(
-        and(
-          eq(employee_time_session.employee_id, id),
-          eq(employee_time_session.status, "terminated")
+    const sessions = (
+      await db
+        .select({
+          date: employee_time_session.date,
+          time: sum(employee_time_session.time),
+        })
+        .from(employee_time_session)
+        .where(
+          and(
+            eq(employee_time_session.employee_id, id),
+            eq(employee_time_session.status, "terminated")
+          )
         )
-      )
-      .groupBy((data) => [data.date])
-      .limit(7);
+        .groupBy((data) => [data.date])
+        .orderBy(asc(employee_time_session.date))
+        .limit(7)
+    ).map((data) => ({ date: data.date, time: Number(data.time) }));
+
+    // padding sessions if needed
+    if (sessions.length !== 7) {
+      const remainig = 7 - sessions.length;
+      const lastDate = sessions[sessions.length - 1].date;
+
+      for (let i = 1; i <= remainig; i++) {
+        const newDate = new Date(lastDate);
+        newDate.setDate(newDate.getDate() + i);
+        sessions.push({ date: newDate, time: 0 });
+      }
+    }
 
     res.status(200).send({ data: sessions, type: "success" });
   } catch (err) {
