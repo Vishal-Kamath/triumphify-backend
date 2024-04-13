b3b7a0209d5c03c0f36f19c1e6d92018d5695b52
Vishal-Kamath <vishalkamath853@gmail.com>
2024-04-13 19:21:05 +0530

    ⬆️update: get employee

--- src/admin/entities/employees/controllers/get.employees..controller.ts
+++ src/admin/entities/employees/controllers/get.employees..controller.ts
@@ -3,7 +3,7 @@ import { Logger } from "@/utils/logger";
 import { db } from "@/lib/db";
 import { employee } from "@/lib/db/schema";
 import { getRole } from "@admin/utils/getRole";
-import { eq, ne } from "drizzle-orm";
+import { and, eq, ne } from "drizzle-orm";
 import { env } from "@/config/env.config";
 import { TokenPayload } from "@/admin/utils/jwt.utils";
 
@@ -14,22 +14,37 @@ const handleGetEmployees = async (
   try {
     const role = getRole(req.body.token.role);
 
-    const condition =
-      role === "superadmin"
-        ? ne(employee.role, env.SUPERADMIN)
-        : eq(employee.role, env.EMPLOYEE);
     const employees = (
-      await db
-        .select({
-          id: employee.id,
-          username: employee.username,
-          email: employee.email,
-          role: employee.role,
-          created_at: employee.created_at,
-          updated_at: employee.updated_at,
-        })
-        .from(employee)
-        .where(condition)
+      role === "superadmin"
+        ? await db
+            .select({
+              id: employee.id,
+              username: employee.username,
+              email: employee.email,
+              role: employee.role,
+              rate: employee.rate,
+              status: employee.status,
+              created_at: employee.created_at,
+              updated_at: employee.updated_at,
+            })
+            .from(employee)
+            .where(ne(employee.role, env.SUPERADMIN))
+        : await db
+            .select({
+              id: employee.id,
+              username: employee.username,
+              email: employee.email,
+              role: employee.role,
+              created_at: employee.created_at,
+              updated_at: employee.updated_at,
+            })
+            .from(employee)
+            .where(
+              and(
+                eq(employee.role, env.EMPLOYEE),
+                eq(employee.status, "active")
+              )
+            )
     ).map((emp) => ({
       ...emp,
       role: getRole(emp.role),
