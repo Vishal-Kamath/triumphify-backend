e65eccbc0334cc082a483d70da2d1e5398620c33
Vishal-Kamath <vishalkamath853@gmail.com>
2024-04-16 14:11:27 +0530

    ⬆️update: employee logs

--- src/admin/entities/employees/controllers/get.log.employees.controller.ts
+++ src/admin/entities/employees/controllers/get.log.employees.controller.ts
@@ -4,19 +4,11 @@ import { Logger } from "@/utils/logger";
 import { Request, Response } from "express";
 
 const handleGetEmployeeLogs = async (
-  req: Request<{ name: string }, {}, {}, { start: string; end: string }>,
+  req: Request<{ name: string }>,
   res: Response
 ) => {
   try {
     const { name } = req.params;
-    const end_date = req.query.end ? new Date(req.query.end) : new Date();
-    const start_date = req.query.start
-      ? new Date(req.query.start)
-      : new Date(
-          new Date(end_date).setMonth(new Date(end_date).getMonth() - 1)
-        );
-    end_date.setHours(23, 59, 59, 999);
-    start_date.setHours(0, 0, 0, 0);
 
     const logs = (await CSVLogger.getLogs(name)) as
       | EmployeeLog[]
@@ -27,17 +19,10 @@ const handleGetEmployeeLogs = async (
         .send({ description: "logs not found", type: "error" });
     }
 
-    const filteredLogs = logs
-      .filter((log) => {
-        return (
-          new Date(log.created_at) >= start_date &&
-          new Date(log.created_at) <= end_date
-        );
-      })
-      .sort(
-        (a, b) =>
-          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-      );
+    const filteredLogs = logs.sort(
+      (a, b) =>
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+    );
 
     res.status(200).send({
       data: filteredLogs,
